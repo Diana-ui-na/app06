f941fe3687862174e2cefa1431602df0d62e87b5
Diana-ui-na <herreramartinezdianamargarita@cetis50cdmx.com>
2024-10-03 19:22:36 -0600

    se sube primera vercion

---
+++ app06/app06/main.py
@@ -0,0 +1,47 @@
+import flet as ft
+import random
+    
+#Funcón principal    
+def main(page: ft.Page):
+    #Variables globales
+    global numero_secreto,entrada_numero,texto_resultado,boton_adivinar
+    
+    page.title = "Adivina el numero"
+
+    #generar numero aleatorio
+    numero_secreto = random.randint(1,100)
+
+    #Crear los elementois de la interfaz
+    titulo=ft.Text("Adivina el número secreto entre 1 y 100",size= 20,color="white")
+    entrada_numero=ft.TextField(label="Tu Adivinanza",width=150)
+    boton_adivinar=ft.ElevatedButton("Adivinar")
+    texto_resultado=ft.Text("",color="white")
+    
+    contenedor_principal=ft.Container(
+        content=ft.Column(
+            controls=[
+                titulo,
+                entrada_numero,
+                boton_adivinar,
+                texto_resultado,
+                ft.Image(
+                    src="https://i.ibb.co/Gxgryg9/laser.gif",
+                    fit=ft.ImageFit.COVER,
+                    width=350,
+                    height=200
+                )
+            ],alignment="CENTER",
+            horizontal_alignment="CENTER",
+            spacing=20
+        ),
+        bgcolor="black",
+        width=page.window.width,
+        height=page.window.height,
+        padding=20
+    ),
+    page.add(contenedor_principal)
+    
+    
+    
+    
+ft.app(main)
